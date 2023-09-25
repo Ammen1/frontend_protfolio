47ec98ad47f9b7a3bc5fb1889a3991879f38fb46
Ammen1 <amenguda@gmail.com>
2023-09-25 19:34:39 +0300

    Portfoloi jsx page

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import "./Portfolio.css";
+// import { Swiper, SwiperSlide } from "swiper/react"
+// import "swiper/css";
+// import Sidebar from "../../img/sidebar.png";
+// import Ecommerce from "../../img/ecommerce.png";
+// import HOC from "../../img/hoc.png";
+// import MusicApp from "../../img/musicapp.png";
+import { themeContext } from "../../Context";
+const Portfolio = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div className="portfolio" id="portfolio">
+      {/* heading */}
+      <span style={{ color: darkMode ? "white" : "" }}>Recent Projects</span>
+      <span>Portfolio</span>
+
+      {/* slider */}
+    </div>
+  );
+};
+
+export default Portfolio;
