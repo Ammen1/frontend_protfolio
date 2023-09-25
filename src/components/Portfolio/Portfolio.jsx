306d2d6440ab36b51840e41e58d4335b2077631e
Ammen1 <amenguda@gmail.com>
2023-09-25 19:35:13 +0300

    services jsx page

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,6 @@
 import React, { useContext } from "react";
 import "./Portfolio.css";
-// import { Swiper, SwiperSlide } from "swiper/react"
-// import "swiper/css";
-// import Sidebar from "../../img/sidebar.png";
-// import Ecommerce from "../../img/ecommerce.png";
-// import HOC from "../../img/hoc.png";
-// import MusicApp from "../../img/musicapp.png";
+
 import { themeContext } from "../../Context";
 const Portfolio = () => {
   const theme = useContext(themeContext);
